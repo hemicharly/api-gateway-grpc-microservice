b32221f3250089dd8408fac114a8a4c8527f8304
Hemicharly <hemicharlythiago@gmail.com>
2020-05-23 22:20:58 -0300

    feat: add class customersServices.ts

---
+++ app/src/services/customersServices.ts
@@ -0,0 +1,55 @@
+import {CustomersRequest, CustomersResponse, CustomersResponseList, Empty} from '../proto/customers/customers_pb';
+import {v4 as uuidv4} from 'uuid';
+
+
+export default class CustomersServices {
+    public static async findAll(): Promise<CustomersResponseList> {
+        const customersResponseList: CustomersResponseList = new CustomersResponseList();
+
+        for (let i: number = 0; i <= 10000; i++) {
+            const customersResponse = new CustomersResponse();
+            customersResponse.setId(uuidv4());
+            customersResponse.setName(uuidv4());
+            customersResponse.setAge(Math.ceil(Math.random() * 10));
+            customersResponse.setAddress(uuidv4());
+            customersResponseList.addCustomersresponselist(customersResponse);
+        }
+
+        return Promise.resolve(customersResponseList);
+    }
+
+    public static async findById(id: string): Promise<CustomersResponse> {
+        const customersResponse = new CustomersResponse();
+        customersResponse.setId(id);
+        customersResponse.setName('Hemicharly Thiago');
+        customersResponse.setAge(27);
+        customersResponse.setAddress('Rua xxx');
+
+        return Promise.resolve(customersResponse);
+    }
+
+    public static async insert(request: CustomersRequest): Promise<CustomersResponse> {
+        const customersResponse = new CustomersResponse();
+        customersResponse.setId(uuidv4());
+        customersResponse.setName(request.getName());
+        customersResponse.setAge(request.getAge());
+        customersResponse.setAddress(request.getAddress());
+
+        return Promise.resolve(customersResponse);
+    }
+
+    public static async update(request: CustomersRequest): Promise<CustomersResponse> {
+        const customersResponse = new CustomersResponse();
+        customersResponse.setId(uuidv4());
+        customersResponse.setName(request.getName());
+        customersResponse.setAge(request.getAge());
+        customersResponse.setAddress(request.getAddress());
+
+        return Promise.resolve(customersResponse);
+    }
+
+    public static async removeById(id: string): Promise<Empty> {
+        console.log('Remove customer id: ', id);
+        return Promise.resolve(new Empty());
+    }
+}
