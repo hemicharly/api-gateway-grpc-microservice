d9ed60fb38ebfc4bcfaa3f138a15929a0adced85
Hemicharly <hemicharlythiago@gmail.com>
2020-05-26 20:58:10 -0300

    fix: add return getall Orders

--- app-orders/src/controllers/orders.controller.go
+++ app-orders/src/controllers/orders.controller.go
@@ -10,8 +10,31 @@ import (
 type server struct{}
 
 func (s *server) GetAll(ctx context.Context, in *pb.Empty) (*pb.OrdersResponseList, error) {
-	var result pb.OrdersResponseList
-	return &result, nil
+
+	var ordersResponseList []*pb.OrdersResponse
+	var ordersResponse pb.OrdersResponse
+
+	ordersResponse.Id = "58d6e7ef-add5-41a7-b661-e2685b19hg7e"
+	ordersResponse.CustomersId = "7a02caa6-b2c2-451e-8cf9-9e994593afe9"
+	ordersResponse.DateOrders = "2020-05-26"
+
+	var ordersItems pb.OrdersItemResponse
+	ordersItems.Id = "58d6e7ef-add5-41a7-b661-e2654gfrc6ca"
+	ordersItems.OrdersId = ordersResponse.Id
+	ordersItems.ProductsId = "48d6e7ef-add5-41a7-b661-e2685b19c6ca"
+	ordersItems.Price = 5
+	ordersItems.Quantity = 2
+	ordersItems.TotalItem = ordersItems.Price * float32(ordersItems.Quantity)
+
+	ordersResponse.OrdersItemResponse = append(ordersResponse.OrdersItemResponse, &ordersItems)
+
+	ordersResponse.TotalOrders = ordersItems.TotalItem
+
+	ordersResponseList = append(ordersResponseList, &ordersResponse)
+
+	return &pb.OrdersResponseList{
+		OrdersResponseList: ordersResponseList,
+	}, nil
 }
 
 func (s *server) Insert(ctx context.Context, ordersRequest *pb.OrdersRequest) (*pb.OrdersResponse, error) {
