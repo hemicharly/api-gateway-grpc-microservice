5cec6250abfb13bcede4b448ac6fad743dcdda1f
Hemicharly <hemicharlythiago@gmail.com>
2020-06-09 22:25:56 -0300

    fix: add start prod entrypoint-server.sh

--- docker/nodejs/entrypoint-server.sh
+++ docker/nodejs/entrypoint-server.sh
@@ -5,7 +5,24 @@ cd /opt/app
 echo "Install packaged..."
 yarn
 
-echo "Start application server..."
-yarn dev
+echo "$NODE_ENV"
+if [ "$NODE_ENV" = "development" ]; then
+  echo "Start development application server..."
+  yarn dev
+fi
+
+if [ "$NODE_ENV" = "production" ]; then
+  echo "Creating paths..."
+  mkdir -p $APP_HOME/logs;
+  mkdir -p $APP_HOME/build;
+
+  yarn clean && yarn build;
+
+  # Copy files *.proto
+  cp -R $APP_HOME/src/config/grpc/proto/ $APP_HOME/build/src/config/grpc/
+
+  echo "Start production application server..."
+  yarn start;
+fi
 
 exec "$@"
