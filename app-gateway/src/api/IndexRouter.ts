1be6a93e6342e3291d5e53f182b2d0c24c18ae0f
Hemicharly <hemicharlythiago@gmail.com>
2020-06-21 11:32:54 -0300

    feat: add router authenticator

--- app-gateway/src/api/IndexRouter.ts
+++ app-gateway/src/api/IndexRouter.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import AuthenticatorRouter from './authenticator/AuthenticatorRouter'
 import CustomersRouter from './customers/CustomersRouter';
 import OrdersRouter from './orders/OrdersRouter';
 import ProductsRouter from './products/ProductsRouter';
@@ -6,6 +7,7 @@ import UsersRouter from './users/UsersRouter';
 
 const router = express.Router();
 
+router.use('/authenticator', AuthenticatorRouter);
 router.use('/customers', CustomersRouter);
 router.use('/orders', OrdersRouter);
 router.use('/products', ProductsRouter);
