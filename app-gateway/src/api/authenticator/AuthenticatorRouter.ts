7d118b160d3e363a3d4d0f2bc5c03b089af055fb
Hemicharly <hemicharlythiago@gmail.com>
2020-06-22 17:30:42 -0300

    fix: remove import not use

--- app-gateway/src/api/authenticator/AuthenticatorRouter.ts
+++ app-gateway/src/api/authenticator/AuthenticatorRouter.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
 import AuthenticatorController from './AuthenticatorController';
 
 const router = express.Router();
